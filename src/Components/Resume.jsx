f45afecd50ae80fa7964cbed9377cc7852d65121
Pratham-Gaikwad <prathamgaikwad2011@gmail.com>
2024-02-12 13:07:55 +0530

    resume timeLine added

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -1,24 +1,64 @@
-import './Resume.css'
-import {Timeline, TimelineEvent} from 'react-event-timeline'
+import './Resume.css'; 
+import { HiAcademicCap , HiBriefcase } from "react-icons/hi";
 
+import {TimeLineElements , TimeLineElements2 } from './TimeLineElements';
+import { VerticalTimeline , VerticalTimelineElement } from 'react-vertical-timeline-component';
+import "react-vertical-timeline-component/style.min.css"
 export default function Resume() {
-
+  let degreeIconStyles = {background : "#06D6A0"};
+  let workIconStyles = {background : "f9c74f"}
   return (
     <div className='resume-main'>
-
-    <h1 className='heading-resume'>My Journey</h1>
-        
-    <div className='edu'>
-    <Timeline lineColor='#84b289'>
-
-            <TimelineEvent title="Higher Secondary Education" createdAt="2018 - 2020" icon={<img className="Graduate.svg"/>}>
-                Text
-            </TimelineEvent>
-            <TimelineEvent title="Btech" createdAt="2021 - Present" icon={<img className="Graduate.svg"/>}>
-                Text
-            </TimelineEvent>
-    </Timeline>
+      <h1 className='heading-resume'>My Journey</h1>
+      <div className='edu'>
+        <VerticalTimeline>
+          {
+            TimeLineElements.map(element =>{
+              let isDegreeIcon = element.icon === "degree";
+              return(
+                <VerticalTimelineElement
+                key ={element.key}
+                date ={element.date}
+                className = 'date'
+                // iconStyle = {isDegreeIcon ? degreeIconStyles : workIconStyles }
+                iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                icon = {isDegreeIcon ? <HiAcademicCap  className='Education'/> : <HiBriefcase className='workexp'/>}
+               >
+                <h4 className='vertical-timeline-element-title'> 
+                {element.title}
+                </h4>
+                <h5 className='vertical-timeline-element-subtitle'> {element.discription}</h5>
+                </VerticalTimelineElement>
+              )
+            })
+          }
+        </VerticalTimeline>
+      </div>
+      <div className='edu2'>
+        <VerticalTimeline>
+          {
+            TimeLineElements2.map(element =>{
+              let isDegreeIcon = element.icon === "degree";
+              return(
+                <VerticalTimelineElement
+                key ={element.key}
+                date ={element.date}
+                className = 'date'
+                // iconStyle = {isDegreeIcon ? degreeIconStyles : workIconStyles }
+                iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                icon = {isDegreeIcon ? <HiAcademicCap  className='Education'/> : <HiBriefcase className='workexp'/>}
+               >
+                <h4 className='vertical-timeline-element-title'> 
+                {element.title}
+                </h4>
+                <h5 className='vertical-timeline-element-subtitle'> {element.discription}</h5>
+                </VerticalTimelineElement>
+              )
+            })
+          }
+        </VerticalTimeline>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
+// <HiAcademicCap /> <HiBriefcase />
---
+++ src/Components/TimeLineElements.js
@@ -0,0 +1,47 @@
+export const TimeLineElements = [
+    {
+        id: 1,
+        title: "Higher Secondary Education",
+        discription: "Vidhya Bahavan Highschool and Jr.College",
+        date: "Aug 2017 - July 2018",
+        icon: "degree"
+    },
+    {
+        id: 1,
+        title: "Higher Secondary Education",
+        discription: "Vidhya Bahavan Highschool and Jr.College",
+        date: "Aug 2017 - July 2018",
+        icon: "degree"
+    },
+    // {
+    //     id: 1,
+    //     title: "Higher Secondary Education",
+    //     discription: "Vidhya Bahavan Highschool and Jr.College",
+    //     date: "Aug 2017 - July 2018",
+    //     icon: "degree"
+    // }
+];
+
+export const TimeLineElements2 = [
+    {
+        id: 1,
+        title: "Higher Secondary Education",
+        discription: "Vidhya Bahavan Highschool and Jr.College",
+        date: "Aug 2017 - July 2018",
+        icon: "degree"
+    },
+    {
+        id: 1,
+        title: "Higher Secondary Education",
+        discription: "Vidhya Bahavan Highschool and Jr.College",
+        date: "Aug 2017 - July 2018",
+        icon: "work"
+    }, 
+    // {
+    //     id: 1,
+    //     title: "Higher Secondary Education",
+    //     discription: "Vidhya Bahavan Highschool and Jr.College",
+    //     date: "Aug 2017 - July 2018",
+    //     icon: "work"
+    // }
+];
